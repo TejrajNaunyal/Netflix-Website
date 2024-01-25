0e8dc92c95db12b3254e5f47005b9616254599bd
TejrajNaunyal <tekrajnaunyal372@gmail.com>
2024-01-25 11:03:20 +0545

    javascript file added

---
+++ index.js
@@ -0,0 +1,46 @@
+
+// Operators
+var num1 = 20;
+var num2 = 25;
+
+var sum = num1 + num2;
+var difference = num1 - num2;
+var product = num1 * num2;
+var quotient = num1 / num2;
+
+// Displaying results
+console.log("Sum: " + sum);
+console.log("Difference: " + difference);
+console.log("Product: " + product);
+console.log("Quotient: " + quotient);
+
+// Control statements
+if (age >= 18) {
+    console.log("You are eligible to vote.");
+} else {
+    console.log("You are not eligible to vote yet.");
+}
+
+// Looping
+console.log("Counting from 1 to 5:");
+for (var i = 1; i <= 5; i++) {
+    console.log(i);
+}
+
+// Function
+function greet(name) {
+    console.log("Hello, " + name + "!");
+}
+
+// Calling the function
+greet("Alice");
+// JavaScript Fundamentals Demo from a W3Schools course materials
+
+// Variables
+var firstName = "Tekraj";
+var lastName = "Naunyal";
+var age = 23;
+
+// Displaying variables
+console.log("Name: " + firstName + " " + lastName);
+console.log("Age: " + age);
